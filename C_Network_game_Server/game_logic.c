d3be5da4a2368049db367f935451f5426b48917d
hxnssc <162525738+hxnssc@users.noreply.github.com>
2025-06-20 18:31:59 +0900

    chore: 리팩토링 및 버그수정
    ENTITY_DEFENDER 관련 버그 수정

--- C_Network_game_Server/game_logic.c
+++ C_Network_game_Server/game_logic.c
@@ -21,7 +21,7 @@ void handle_join(SOCKET client_fd, PayloadJoin* payload) {
     }
 
     // 방어자 중복 체크
-    if (type == ENTITY_PLAYER && has_player) {
+    if (type == ENTITY_DEFENDER && has_player) {
         printf("Server> 이미 방어자가 존재합니다. 요청 거부.\n");
 
         MsgHeader h = { .type = MSG_GAME_EVENT, .length = htonl(sizeof(PayloadGameEvent)) };
@@ -40,10 +40,10 @@ void handle_join(SOCKET client_fd, PayloadJoin* payload) {
         return;
     }
 
-    if (type == ENTITY_PLAYER) has_player = 1;
+    if (type == ENTITY_DEFENDER) has_player = 1;
 
     printf("Server> JOIN: client_fd=%d → entity_id=%u [%s]\n", client_fd, ent->entity_id,
-        type == ENTITY_DEFENDER ? "PLAYER" : "ATTACKER");
+        type == ENTITY_DEFENDER ? "DEFENDER" : "ATTACKER");
 
     // 클라이언트에게 entity_id를 응답으로 보냄
     PayloadJoinAck ackPayload;
