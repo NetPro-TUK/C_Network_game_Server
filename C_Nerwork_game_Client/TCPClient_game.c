4d26113ad9bfca1644d86f031fe07ce3abf95115
kwonbum026 <kwonbum026@gmail.com>
2025-06-20 17:05:39 +0900

    Update TCPClient_game.c

--- C_Nerwork_game_Client/TCPClient_game.c
+++ C_Nerwork_game_Client/TCPClient_game.c
@@ -1,18 +1,25 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
 
 #include "protocol.h"
 #include "net_utils.h"
 #include "log.h"
 
+#pragma comment(lib, "Ws2_32.lib")
+
+#define FIELD_WIDTH 80
+#define FIELD_HEIGHT 25
+#define PLAYER_CHAR 'A'
+
+void gotoxy(int x, int y);
+void hide_cursor();
+void draw_player(int x, int y);
+void erase_player(int x, int y);
 void ErrorHandling(char* message);
-#define MAX_PACKET_SIZE  120
-
-#define ENTITY_PLAYER 1
-#define ENTITY_ATTACKER 2
-
 
 int main(void)
 {
@@ -32,27 +39,14 @@ int main(void)
     servAdr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servAdr.sin_port = htons(9000);
 
-    int ret = connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr));
-    if (ret == SOCKET_ERROR) {
+    if (connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
         printf("<ERROR> Client. connect() 실행 오류.\n");
         closesocket(hSocket);
         WSACleanup();
         return 0;
     }
 
-    printf("Client> connection established...\n");
-
-    // -----------------------------
-    // 1. 역할 선택 및 MSG_JOIN 전송
-    // -----------------------------
-    int role;
-    printf("역할 선택: (1) 방어자, (2) 공격자 > ");
-    scanf("%d", &role);
-    if (role != 1 && role != 2) {
-        printf("잘못된 입력. 기본값: 방어자(1)\n");
-        role = 1;
-    }
-
+    int role = 1; // 방어자 고정
     PayloadJoin joinPayload = { .role = role };
     MsgHeader joinHeader = {
         .type = MSG_JOIN,
@@ -61,11 +55,7 @@ int main(void)
 
     send_full(hSocket, &joinHeader, sizeof(joinHeader));
     send_full(hSocket, &joinPayload, sizeof(joinPayload));
-    printf("Client> 역할 선택 전송 완료 (%s)\n", role == 1 ? "방어자" : "공격자");
 
-    // -----------------------------
-    // 2. MSG_JOIN_ACK 응답 수신
-    // -----------------------------
     MsgHeader ackHeader;
     recv_full(hSocket, &ackHeader, sizeof(ackHeader));
     if (ackHeader.type != MSG_JOIN_ACK || ntohl(ackHeader.length) != sizeof(PayloadJoinAck)) {
@@ -76,55 +66,79 @@ int main(void)
     PayloadJoinAck ackPayload;
     recv_full(hSocket, &ackPayload, sizeof(ackPayload));
     uint32_t myId = ntohl(ackPayload.entityId);
-    printf("Client> 내 Entity ID = %u, 역할 = %s\n", myId,
-        ackPayload.role == ENTITY_PLAYER ? "PLAYER" : "ATTACKER");
 
-    // -----------------------------
-    // 3. 테스트용 MSG_STATE_UPDATE 전송
-    // -----------------------------
-    PayloadStateUpdate statePayload;
-    statePayload.entityId = htonl(myId);
-    statePayload.x = 10;
-    statePayload.y = 20;
+    int player_x = 10;
+    int player_y = FIELD_HEIGHT / 2;
 
-    MsgHeader stateHeader = {
-        .type = MSG_STATE_UPDATE,
-        .length = htonl(sizeof(statePayload))
-    };
+    hide_cursor();
+    system("cls");
+    draw_player(player_x, player_y);
+    gotoxy(0, FIELD_HEIGHT);
+    printf("↑ ↓ 방향키로 이동. ESC로 종료\n");
 
-    send_full(hSocket, &stateHeader, sizeof(stateHeader));
-    send_full(hSocket, &statePayload, sizeof(statePayload));
-    printf("Client> 상태 전송 완료\n");
+    while (1) {
+        if (_kbhit()) {
+            int key = _getch();
+            if (key == 27) break;
 
-    // -----------------------------
-    // 4. 테스트용 총알 발사(MSG_ACTION_EVENT) 전송
-    // -----------------------------
-    PayloadActionEvent actionPayload;
-    actionPayload.shooterId = htonl(myId);
-    actionPayload.bulletId = htonl(999);  // 테스트용 Bullet ID
-    actionPayload.dirX = 1;               // 오른쪽 방향
-    actionPayload.dirY = 0;
+            if (key == 224) {
+                key = _getch();
+                erase_player(player_x, player_y);
+                if (key == 72 && player_y > 0) player_y--; // ↑
+                else if (key == 80 && player_y < FIELD_HEIGHT - 1) player_y++; // ↓
+                draw_player(player_x, player_y);
 
-    MsgHeader actionHeader = {
-        .type = MSG_ACTION_EVENT,
-        .length = htonl(sizeof(actionPayload))
-    };
+                // 서버로 상태 전송
+                PayloadStateUpdate statePayload = {
+                    .entityId = htonl(myId),
+                    .x = player_x,
+                    .y = player_y
+                };
+                MsgHeader stateHeader = {
+                    .type = MSG_STATE_UPDATE,
+                    .length = htonl(sizeof(statePayload))
+                };
+                send_full(hSocket, &stateHeader, sizeof(stateHeader));
+                send_full(hSocket, &statePayload, sizeof(statePayload));
+            }
+        }
 
-    send_full(hSocket, &actionHeader, sizeof(actionHeader));
-    send_full(hSocket, &actionPayload, sizeof(actionPayload));
-    printf("Client> 총알 발사 전송 완료 (bulletId = 999)\n");
+        Sleep(50);
+    }
 
-    // -----------------------------
-    // 5. 종료
-    // -----------------------------
     closesocket(hSocket);
-    printf("Client> close socket...\n");
+    CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+    cursorInfo.bVisible = 1;
+    cursorInfo.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+    printf("\nClient> close socket...\n");
     WSACleanup();
     return 0;
 }
 
-void ErrorHandling(char* message)
-{
+void gotoxy(int x, int y) {
+    COORD pos = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void hide_cursor() {
+    CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+    cursorInfo.bVisible = 0;
+    cursorInfo.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+void draw_player(int x, int y) {
+    gotoxy(x, y);
+    putchar(PLAYER_CHAR);
+}
+
+void erase_player(int x, int y) {
+    gotoxy(x, y);
+    putchar(' ');
+}
+
+void ErrorHandling(char* message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
